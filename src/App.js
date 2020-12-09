03045075e036e91a0b717381cfe7236ae835ace7
Lisa Poncet <poncet.lisa@gmail.com>
2020-12-09 16:07:15 +0100

    add delete button on complete

--- src/App.js
+++ src/App.js
@@ -6,42 +6,61 @@ function App() {
   const [input, setInput] = useState("");
   const [items, setItems] = useState([]);
   const [filteredItems, setFilteredItems] = useState([]);
-  const [filter, setFilter] = useState('all')
+  const [filter, setFilter] = useState("all");
 
-  const addItem = (input) => {
-    let newItem = {
-      label: input,
-      id: Math.floor(Math.random() * 100),
-      isCompleted: false,
+  const addItem = (item) => {
+    if (item) {
+      const createId = () => {
+        let id = Math.floor(Math.random() * 100);
+        if (items.find((e) => e.id === id))
+          id = Math.floor(Math.random() * 100);
+        return id;
+      };
+      let newItem = {
+        label: item,
+        id: createId(),
+        isCompleted: false,
+      };
+      setItems((items) => [...items, newItem]);
+      setInput("");
     }
-    setItems((items) => [...items, newItem]);
   };
 
   const completeItem = (id) => {
-      let index = items.findIndex(e => e.id === id)
-      let modItems = [...items];
-      modItems[index].isCompleted = !modItems[index].isCompleted 
-      setItems( modItems );
+    let index = items.findIndex((e) => e.id === id);
+    let modItems = [...items];
+    modItems[index].isCompleted = !modItems[index].isCompleted;
+    setItems(modItems);
   };
 
-  const handleFilter = (status) =>  {
-    if(status === 'active') setFilteredItems(items.filter(e => !e.isCompleted));
-    else if(status === 'completed') setFilteredItems(items.filter(e => e.isCompleted));
+  const handleFilter = (status) => {
+    if (status === "active")
+      setFilteredItems(items.filter((e) => !e.isCompleted));
+    else if (status === "completed")
+      setFilteredItems(items.filter((e) => e.isCompleted));
     setFilter(status);
   };
 
-  const displayedItems = filter === 'all' ? items : filteredItems;
+  const displayedItems = filter === "all" ? items : filteredItems;
 
   return (
     <div className="App">
       <h1>#todo</h1>
-      <button onClick={() => handleFilter('all')}>All</button>
-      <button onClick={() => handleFilter('active')}>Active</button>
-      <button onClick={() => handleFilter('completed')}>Completed</button>
-      <hr/>
-      <input onChange={(event) => setInput(event.target.value)} type="text" />
+      <button onClick={() => handleFilter("all")}>All</button>
+      <button onClick={() => handleFilter("active")}>Active</button>
+      <button onClick={() => handleFilter("completed")}>Completed</button>
+      <hr />
+      <input
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        type="text"
+      />
       <button onClick={() => addItem(input)}>add</button>
-      <ItemList items={displayedItems} handleComplete={completeItem} />
+      <ItemList
+        items={displayedItems}
+        tab={filter}
+        handleComplete={completeItem}
+      />
     </div>
   );
 }
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -7,7 +7,12 @@ const ItemList = (props) => {
   return (
     <ul className="items-list">
       {items.map((item) => (
-        <Item key={item.id} item={item} handleComplete={props.handleComplete} />
+        <Item
+          key={item.id}
+          tab={props.tab}
+          item={item}
+          handleComplete={props.handleComplete}
+        />
       ))}
     </ul>
   );
